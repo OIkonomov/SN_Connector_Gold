1675bc1171e8d89ee8164eb48bd79e875b01fb11
Kristiyan-Nikolov <73643834+Kristiyan-Nikolov@users.noreply.github.com>
2020-11-09 13:27:31 +0200

    Created Launch Resume for WPO

---
+++ Scripts/WPO_Launch_Resume.py
@@ -0,0 +1,12 @@
+TYPE = "Player"
+CREDENTIAL = "YES"
+FILTER = "NO"
+SILO = "NO"
+REALM = "NO"
+DATE = "YES"
+
+SQL_REQ = '''
+SELECT * FROM "ELEPHANT_DB"."WPO"."LAUNCH_RESUME"
+WHERE DATE(SERVER_TIME) >= '{st_date}' AND DATE(SERVER_TIME) <= '{end_date}' AND FED_ID = '{account}'
+ORDER BY SERVER_TIME;
+'''
