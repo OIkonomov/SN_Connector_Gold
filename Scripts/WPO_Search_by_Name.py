d8e40efdf564e66e948fe07330bbf95d0ab1e289
Kristiyan-Nikolov <73643834+Kristiyan-Nikolov@users.noreply.github.com>
2021-05-27 15:16:25 +0300

    Add files via upload

--- Scripts/WPO_Search_by_Name.py
+++ Scripts/WPO_Search_by_Name.py
@@ -6,7 +6,18 @@ REALM = "NO"
 DATE = "YES"
 
 SQL_REQ = '''
-SELECT * FROM "ELEPHANT_DB"."WPO"."PLAYER_CONNECTION_REPORT"
-WHERE DATE(SERVER_TIME) >= '{st_date}' AND DATE(SERVER_TIME) <= '{end_date}' and INGAME_NICKNAME_ACTIVE LIKE LOWER('%{filter_value}%')
+SELECT 
+FED_ID as "fed id",
+SERVER_TIME as "Server time",
+EVENT_DATA:player_alias::STRING as "Gloft id",
+EVENT_DATA:player_name::STRING as "Name",
+EVENT_DATA:progress_index::STRING as "Progress index",
+EVENT_DATA:realm_id:STRING as "Realm"
+FROM "TRACKING_DB"."WPO"."GOLD" 
+WHERE EVENT_ID = '51904' and DATE(SERVER_TIME) >= '{st_date}' AND DATE(SERVER_TIME) <= '{end_date}' and EVENT_DATA:player_name::STRING like '%{filter_value}%'
 ORDER BY SERVER_TIME;
+
+
+
+
 '''
