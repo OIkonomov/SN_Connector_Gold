e1791e1204fa32f1d50a2d72c3c4865924936870
OIkonomov <o.ikonomov@gmail.com>
2021-07-08 14:38:09 +0300

    new script

---
+++ Scripts/AoV_IAP_PACKS_PURCHASED.py
@@ -0,0 +1,21 @@
+TYPE = "Player"
+CREDENTIAL = "YES"
+FILTER = "NO"
+SILO = "NO"
+REALM = "NO"
+DATE = "YES"
+
+SQL_REQ =   '''
+
+SELECT
+    *
+FROM "ELEPHANT_DB"."AOV"."IAP_PACKS_PURCHASED"
+
+WHERE
+    SERVER_TIME >= '{st_date}'
+    AND SERVER_TIME < '{end_date}'
+    AND FED_ID = '{account}'
+
+LIMIT 10000
+;
+'''
